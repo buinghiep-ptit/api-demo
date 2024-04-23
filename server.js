fde200cd8327eb39856325290c47eae0f9f51c41
NGHIEP BUI <nghiepbv2@fpt.com.vn>
2024-04-23 17:26:22 +0700

    add

---
+++ server.js
@@ -0,0 +1,87 @@
+const fs = require("fs");
+const https = require("https");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 8000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const sessions = {};
+
+const db = {
+  users: [{ id: 1, email: "nguyenvana@gmail.com", password: "123456" }],
+  posts: [
+    { id: 1, title: "Post 1" },
+    { id: 2, title: "Post 2" },
+    { id: 3, title: "Post 3" },
+  ],
+};
+
+// [GET] /
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+// [GET] /api/posts
+app.get("/api/posts", (req, res) => {
+  res.json(db.posts);
+});
+
+// [POST] /api/auth/login
+app.post("/api/auth/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = db.users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const sessionId = Math.random().toString(36).substring(7);
+  sessions[sessionId] = { userId: user.id };
+  res
+    .setHeader(
+      "Set-Cookie",
+      `sessionId=${sessionId}; HttpOnly; Max-age=3600; SameSite=None; Secure`
+    )
+    .json(user);
+});
+
+// [GET] /api/auth/me
+app.get("/api/auth/me", (req, res) => {
+  const sessionId = req.cookies.sessionId;
+  const session = sessions[sessionId];
+  console.log(session);
+  if (!session) {
+    return res.status(401).json({ message: "Unauthenticated" });
+  }
+
+  const user = db.users.find((user) => user.id === session.userId);
+  res.json(user);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("testcookie.com+2-key.pem"),
+//       cert: fs.readFileSync("testcookie.com+2.pem"),
+//     },
+//     app
+//   )
+//   .listen(port, () => {
+//     console.log(`Server is running on https://localhost:${port}`);
+//   });
